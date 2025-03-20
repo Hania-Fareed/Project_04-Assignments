d57b282f8c636542c2b3bc3a69f8e1ae70d316e0
Hania-Fareed <haniafareed7@gmail.com>
2025-03-20 21:58:41 +0500

    Add files via upload

---
+++ 00_intro_python/square_number.py
@@ -0,0 +1,7 @@
+def main():
+    number : float = float(input("Type a number to see its square: "))
+    total = number ** 2
+    print(f"square of {number} is {total:.2f}")
+
+if __name__ == '__main__':
+    main()    
