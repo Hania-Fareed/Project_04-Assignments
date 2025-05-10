07e615f36547914cb56f45c823783006e9cb294c
Hania-Fareed <haniafareed7@gmail.com>
2025-05-11 01:44:03 +0500

    Add files via upload

---
+++ 02_lists/01_add_many_number.py
@@ -0,0 +1,10 @@
+def main() :
+    numberList : int = [1, 2, 3, 4, 5]
+    sumofnumbers : int = sum(numberList)
+    print (sumofnumbers)    
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ 02_lists/02_double_list.py
@@ -0,0 +1,9 @@
+def main():
+    elements : int = [1, 2, 3, 4, 5]
+
+    double_elements = [i * 2 for i in elements]
+    print(double_elements)
+
+if __name__ == '__main__':
+    main()
+
